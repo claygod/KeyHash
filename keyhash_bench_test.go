54849ecc921eff7c8307fe40b252fb880415a98f
Eduard <claygod@yandex.ru>
2016-08-08 16:20:04 +0300

    Add files via upload

--- keyhash_bench_test.go
+++ keyhash_bench_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-const ADD_PATH_COUNT = 1000000
+const ADD_PATH_COUNT = 500001
 
 // Test claygod/KeyHash Min ns/op
 func BenchmarkKeyHashMin(b *testing.B) {
@@ -39,73 +39,58 @@ func BenchmarkKeyHashMax(b *testing.B) {
 func BenchmarkKeyHashShortStringKey(b *testing.B) {
 	b.StopTimer()
 	m := make(map[string]int)
-	var a int
-	var z int
 	for i := ADD_PATH_COUNT; i > 0; i-- {
 		x := strconv.Itoa(i)
 		m[x] = i
 	}
 	b.StartTimer()
 	for i := ADD_PATH_COUNT; i > 0; i-- {
-		z = 0
-		a = m["1"]
-		a += m["1000"]
-		a += m["5000"]
-		a += m["9999"]
-		a += m["100000"]
-		a += m["500000"]
-		a += m["999999"]
-		z = a
+		m["2"] = m["1"]
+		m["1000"] = m["1001"]
+		m["5000"] = m["5001"]
+		m["9999"] = m["9991"]
+		m["100000"] = m["100001"]
+		m["200000"] = m["200001"]
+
 	}
-	a = z
 }
 
 // Test claygod/KeyHash Long String key ns/op
 func BenchmarkKeyHashLongStringKey(b *testing.B) {
 	b.StopTimer()
 	m := make(map[string]int)
-	var a int
-	var z int
 	for i := ADD_PATH_COUNT; i > 0; i-- {
 		x := strconv.Itoa(i) + " this very long strring key"
 		m[x] = i
 	}
 	b.StartTimer()
 	for i := ADD_PATH_COUNT; i > 0; i-- {
-		z = 0
-		a = m["1 this very long strring key"]
-		a += m["1000 this very long strring key"]
-		a += m["5000 this very long strring key"]
-		a += m["9999 this very long strring key"]
-		a += m["100000 this very long strring key"]
-		a += m["500000 this very long strring key"]
-		a += m["999999 this very long strring key"]
-		z = a
+		m["2 this very long strring key"] = m["1 this very long strring key"]
+		m["1000 this very long strring key"] = m["1001 this very long strring key"]
+		m["5000 this very long strring key"] = m["5001 this very long strring key"]
+		m["9999 this very long strring key"] = m["9991 this very long strring key"]
+		m["100000 this very long strring key"] = m["100001 this very long strring key"]
+		m["200000 this very long strring key"] = m["200001 this very long strring key"]
+
 	}
-	a = z
 }
 
 // Test claygod/KeyHash uint64 key ns/op
 func BenchmarkKeyHashUint64Key(b *testing.B) {
 	b.StopTimer()
 	m := make(map[uint64]int)
-	var a int
-	var z int
 	for i := ADD_PATH_COUNT; i > 0; i-- {
 		x := uint64(i)
 		m[x] = i
 	}
 	b.StartTimer()
 	for i := ADD_PATH_COUNT; i > 0; i-- {
-		z = 0
-		a = m[1]
-		a += m[1000]
-		a += m[5000]
-		a += m[9999]
-		a += m[100000]
-		a += m[500000]
-		a += m[999999]
-		z = a
+		m[2] = m[1]
+		m[1000] = m[1001]
+		m[5000] = m[5001]
+		m[9999] = m[9991]
+		m[100000] = m[100001]
+		m[200000] = m[200001]
+
 	}
-	a = z
 }
