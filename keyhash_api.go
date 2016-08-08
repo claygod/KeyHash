46a2ed1b1d86469f37983773d5f2ff6053867130
Eduard <claygod@yandex.ru>
2016-08-08 15:47:07 +0300

    Update keyhash_api.go

--- keyhash_api.go
+++ keyhash_api.go
@@ -3,7 +3,7 @@
 package KeyHash
 
 // KeyHash
-// Work
+// API
 
 import (
 	"crypto/md5"
@@ -16,6 +16,7 @@ func New() *KeyHash {
 
 const multiplier uint64 = 1024
 
+// KeyHash structure
 type KeyHash struct{}
 
 // Min - receiving a hash of a string up to 8 characters in length.
